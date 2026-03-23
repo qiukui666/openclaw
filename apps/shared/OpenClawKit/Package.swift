af7c30db2fe794ce14916157608183655aa5603f
root <root@DESKTOP-LK0UM3A.localdomain>
2026-03-23 19:00:02 +0800

    fix(ios-lite): lower OpenClawKit iOS platform to v16 to match lite target

--- apps/shared/OpenClawKit/Package.swift
+++ apps/shared/OpenClawKit/Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "OpenClawKit",
     platforms: [
-        .iOS(.v18),
+        .iOS(.v16),
         .macOS(.v15),
     ],
     products: [
